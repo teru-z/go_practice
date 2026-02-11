94b1b175508358b586f3c7ebe674347ccb7bc8cc
Teru <26590954+Terunori-Hashizume@users.noreply.github.com>
2026-02-11 12:17:05 +0900

    10章3 v2.0.0

--- learning_go/prob-ch10/go.mod
+++ learning_go/prob-ch10/go.mod
@@ -1,3 +1,5 @@
 module prob-ch10
 
 go 1.25.6
+
+require golang.org/x/exp v0.0.0-20260209203927-2842357ff358
--- learning_go/prob-ch10/main.go
+++ learning_go/prob-ch10/main.go
@@ -1,8 +1,14 @@
 package main
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add 関数は、2つの整数 a と b を受け取り、それらの合計を返します。
 // より詳しい情報については、次のリンクを参照してください:
 // https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
