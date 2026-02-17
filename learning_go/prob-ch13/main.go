7a6fd892d8a35fa7e7564db6ea753ed7246f8729
Teru <26590954+Terunori-Hashizume@users.noreply.github.com>
2026-02-17 21:19:12 +0900

    13章2完了

--- learning_go/prob-ch13/main.go
+++ learning_go/prob-ch13/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -10,13 +12,36 @@ func getCurrentTimeInRFC3339() string {
 	return time.Now().Format(time.RFC3339)
 }
 
+func LoggingMiddleware(logger *slog.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			logger.Info("request received",
+				"method", r.Method,
+				"path", r.URL.Path,
+				"remote", r.RemoteAddr,
+				"user_agent", r.UserAgent(),
+			)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
 func run() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	logger := slog.New(
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		}),
+	)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
 			fmt.Fprint(w, getCurrentTimeInRFC3339())
 		}
 	})
-	http.ListenAndServe(":8080", nil)
+
+	handler := LoggingMiddleware(logger)(mux)
+	http.ListenAndServe(":8080", handler)
 }
 
 func main() {
