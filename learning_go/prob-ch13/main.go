f92c85096440f7e83932b9772d0736717fd3251e
Teru <26590954+Terunori-Hashizume@users.noreply.github.com>
2026-02-18 21:58:54 +0900

    13章3完了

--- learning_go/prob-ch13/main.go
+++ learning_go/prob-ch13/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -12,11 +13,43 @@ func getCurrentTimeInRFC3339() string {
 	return time.Now().Format(time.RFC3339)
 }
 
+type NowJSON struct {
+	DayOfWeek  string `json:"day_of_week"`
+	DayOfMonth int    `json:"day_of_month"`
+	Month      int    `json:"month"`
+	Year       int    `json:"year"`
+	Hour       int    `json:"hour"`
+	Minute     int    `json:"minute"`
+	Second     int    `json:"second"`
+}
+
+func getCurrentTimeInJSONString() string {
+	now := time.Now()
+
+	v := NowJSON{
+		DayOfWeek:  now.Weekday().String(), // "Monday" など
+		DayOfMonth: now.Day(),
+		Month:      int(now.Month()),
+		Year:       now.Year(),
+		Hour:       now.Hour(),
+		Minute:     now.Minute(),
+		Second:     now.Second(),
+	}
+
+	b, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(b)
+}
+
 func LoggingMiddleware(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			logger.Info("request received",
 				"method", r.Method,
+				"header", r.Header,
 				"path", r.URL.Path,
 				"remote", r.RemoteAddr,
 				"user_agent", r.UserAgent(),
@@ -36,7 +69,11 @@ func run() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
-			fmt.Fprint(w, getCurrentTimeInRFC3339())
+			if r.Header.Get("Accept") == "application/json" {
+				fmt.Fprint(w, getCurrentTimeInJSONString())
+			} else {
+				fmt.Fprint(w, getCurrentTimeInRFC3339())
+			}
 		}
 	})
 
